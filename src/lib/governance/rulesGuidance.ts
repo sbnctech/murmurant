e18c135921467a09b5beefb139348678c0373829
Edward A. Forman <forman.ed@gmail.com>
2025-12-16 13:19:40 -0800

    chore: add stub modules for governance and meetings

---
+++ src/lib/governance/rulesGuidance.ts
@@ -0,0 +1,8 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
+export async function createRulesGuidance(_p: any): Promise<any> { throw new Error("Not implemented"); }
+export async function listRulesGuidance(_p: any): Promise<any> { throw new Error("Not implemented"); }
+export async function searchRulesGuidance(_q: string): Promise<any> { throw new Error("Not implemented"); }
+export async function getGuidanceCategories(): Promise<any> { throw new Error("Not implemented"); }
+export async function getRulesGuidance(_id: string): Promise<any> { throw new Error("Not implemented"); }
+export async function updateRulesGuidance(_id: string, _p: any): Promise<any> { throw new Error("Not implemented"); }
+export async function deleteRulesGuidance(_id: string): Promise<any> { throw new Error("Not implemented"); }
---
+++ src/lib/meetings/meetings.ts
@@ -0,0 +1,7 @@
+/* eslint-disable @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars */
+export async function createMeeting(_p: any): Promise<any> { throw new Error("Not implemented"); }
+export async function listMeetings(_p: any): Promise<any> { throw new Error("Not implemented"); }
+export async function getMeetingsNeedingMinutes(): Promise<any> { throw new Error("Not implemented"); }
+export async function getMeeting(_id: string): Promise<any> { throw new Error("Not implemented"); }
+export async function updateMeeting(_id: string, _p: any): Promise<any> { throw new Error("Not implemented"); }
+export async function deleteMeeting(_id: string): Promise<any> { throw new Error("Not implemented"); }
