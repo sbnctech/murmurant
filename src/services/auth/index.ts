46280c1ebd10a1ccdb1cf5b914971562aa66d13e
Edward A. Forman <forman.ed@gmail.com>
2025-12-27 23:40:27 -0800

    feat(phase0): add native auth service abstraction

---
+++ src/services/auth/index.ts
@@ -0,0 +1,9 @@
+export type { AuthService } from "./AuthService";
+export type {
+  AuthUser,
+  AuthSession,
+  LoginCredentials,
+  RegisterData,
+} from "./types";
+export { NativeAuthService } from "./NativeAuthService";
+export { MockAuthService } from "./MockAuthService";
